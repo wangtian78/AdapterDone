4384f3f44fc25f3be3ae2b1a166a342b7c1610ae
Rick Mercer <mercer@cs.arizona.edu>
2016-10-13 17:44:01 -0700

    Refactored code and moved a file

--- src/tests/StudentCollectionTest.java
+++ src/tests/StudentCollectionTest.java
@@ -4,11 +4,11 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import _todoInSection.StudentCollection;
 import model.Student;
-import model.StudentCollection;
 
 //
-// A unit test for StudentCollection, okay a small start
+// A unit test for StudentCollection, okay just a small start
 //
 // @author Rick Mercer
 //
@@ -16,12 +16,11 @@ public class StudentCollectionTest {
 
   @Test
   public void testGetters() {
-    StudentCollection students = new StudentCollection(false);
-    assertEquals(0, students.size());
-    students.add(new Student("Dakota", "Psychology", 1.999, 22));
-    students.add(new Student("Kim", "Computer Science", 2.0, 23));
-    assertEquals(2, students.size());
+    StudentCollection students = new StudentCollection();
+    students.add(new Student("Dakota", "Psychology", 1.99, 22));
+    students.add(new Student("Jamie", "Computer Science", 2.0, 23));
     assertNull(students.getFirstStudentWithName("Not here"));
-    assertEquals(1.999, students.getFirstStudentWithName("Dakota").getGPA(), 0.0001);
+    assertEquals(1.99, students.getFirstStudentWithName("Dakota").getGPA(), 0.0001);
+    assertEquals(2.0, students.getFirstStudentWithName("Jamie").getGPA(), 0.0001);
   }
 }
