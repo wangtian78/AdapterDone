129d6d35ed43949bf488d4dad41dcc822c0d15ad
wangtian78 <wangtian@pharmacy.arizona.edu>
2016-10-15 04:01:05 +0100

    Adaptor done

--- src/_todoInSection/StudentCollection.java
+++ src/_todoInSection/StudentCollection.java
@@ -3,6 +3,9 @@ package _todoInSection;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+
 import model.Student;
 
 /**
@@ -16,7 +19,7 @@ import model.Student;
 // (after adding implements TableModel to the class heading).
 //
 // Note: Some TableModel methods need not be implemented.
-public class StudentCollection  {
+public class StudentCollection implements TableModel {
 
   private List<Student> theStudents;
 
@@ -61,5 +64,79 @@ public class StudentCollection  {
     }
     return null; // not found
   }
+
+@Override
+public void addTableModelListener(TableModelListener arg0) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public Class<?> getColumnClass(int col) {
+	if (col==0||col==1)
+		return String.class;
+	if (col==2)
+		return Double.class;
+	if (col==3)
+		return int.class;
+	return null;
+}
+
+@Override
+public int getColumnCount() {
+	// TODO Auto-generated method stub
+	return 4;
+}
+
+@Override
+public String getColumnName(int col) {
+	if (col==0)
+		return "Name";
+	if (col==1)
+		return "Major";
+	if (col==2)
+		return "GPA";
+	if (col==3)
+		return "Age";
+	return null;
+}
+
+@Override
+public int getRowCount() {
+	// TODO Auto-generated method stub
+	return theStudents.size();
+}
+
+@Override
+public Object getValueAt(int row, int col) {
+	Student temp = theStudents.get(row);
+	if (col==0)
+		return temp.getName();
+	if (col==1)
+		return temp.getMajor();
+	if (col==2)
+		return temp.getGPA();
+	if (col==3)
+		return temp.getAge();
+	return null;
+}
+
+@Override
+public boolean isCellEditable(int arg0, int arg1) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+@Override
+public void removeTableModelListener(TableModelListener arg0) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void setValueAt(Object arg0, int arg1, int arg2) {
+	// TODO Auto-generated method stub
+	
+}
 
 }
--- src/_todoInSection/StudentGUI.java
+++ src/_todoInSection/StudentGUI.java
@@ -8,79 +8,91 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.RowSorter;
 import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
 @SuppressWarnings("serial")
 public class StudentGUI extends JFrame {
 
-  /**
-   * Set the GUI to be visible
-   * 
-   * @param args
-   *            unused
-   */
-  public static void main(String args[]) {
-    new StudentGUI().setVisible(true);
-  }
+	/**
+	 * Set the GUI to be visible
+	 * 
+	 * @param args
+	 *            unused
+	 */
+	public static void main(String args[]) {
+		new StudentGUI().setVisible(true);
+	}
 
-  // Need a TableModel to set as the model for a JTabel
-  private TableModel model = null;
+	// Need a TableModel to set as the model for a JTabel
+	private TableModel model = null;
 
-  // Like DefaultListModel and JList, now we have
-  // a class that implements TableModel so this JTable
-  // can display rows and columns of data in a graphical manner
-  private JTable table = null;
+	// Like DefaultListModel and JList, now we have
+	// a class that implements TableModel so this JTable
+	// can display rows and columns of data in a graphical manner
+	private JTable table = null;
 
-  /**
-   * The constructor for a StudentTable. Sets up the GUI and the JTable
-   */
-  public StudentGUI() {
-    // set up the JFrame
-    setTitle("Sort Table Demo");
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setSize(700, 140);
-    setLocation(30, 30);
+	/**
+	 * The constructor for a StudentTable. Sets up the GUI and the JTable
+	 */
+	public StudentGUI() {
+		// set up the JFrame
+		setTitle("Sort Table Demo");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(700, 140);
+		setLocation(30, 30);
 
-    // TODO: 2) Need a new StudentCollection as our model
- 
-    // TODO: 3) Construct the JTable (table) with our model as an argument (could use setModel)
- 
-    // TODO: 4) Construct a JScrollPane to decorate table so that if the data exceeds the 
-    // side of the table in the  GUI, then it automatically becomes scrollable.
-    
-    // TODO: 5) Add JScrollPane to this JFrame
-   
-    
-    // TODO: Run this code to see if the JTable appears (no code to write)
+		// TODO: 2) Need a new StudentCollection as our model
+		model = new StudentCollection();
+		// TODO: 3) Construct the JTable (table) with our model as an argument
+		// (could use setModel)
 
-    
-    // TODO: 6) Construct a new RowSorter<TableModel> to be a TableRowSorter
-    // while setting its model to model
- 
-    // TODO: 7) Link up table and the sorter
- 
-    // Layout the GUI
-    JButton button = new JButton("Select Highlighted Row");
-    JPanel panel = new JPanel();
-    panel.setMaximumSize(new Dimension(30, 30));
-    panel.add(button);
-    add(panel, BorderLayout.WEST);
+		table = new JTable(model);
+//		table.setAutoCreateRowSorter(true);
 
-    // Listen to the button click
-    button.addActionListener(new ButtonListener());
-  }
+		// TODO: 4) Construct a JScrollPane to decorate table so that if the
+		// data exceeds the
+		// side of the table in the GUI, then it automatically becomes
+		// scrollable.
+		JScrollPane scroPane = new JScrollPane(table);
+		// TODO: 5) Add JScrollPane to this JFrame
+		this.add(scroPane);
 
-  private class ButtonListener implements ActionListener {
+		// TODO: Run this code to see if the JTable appears (no code to write)
 
-    @Override
-    public void actionPerformed(ActionEvent arg0) {
-      // TODO: 8) Show the name of the student on the currently selected
-      // row need table's getSelectedRow and convertRowIndexToModel as
-      // well as model's getValueAt(rowIndex, columnIndex). See the API
-      // for details.
-      System.out.println("Get student's name from the table");
+		// TODO: 6) Construct a new RowSorter<TableModel> to be a TableRowSorter
+		// while setting its model to model
+		RowSorter<TableModel> sorter = new TableRowSorter<>(table.getModel());
+		
+		// TODO: 7) Link up table and the sorter
+		table.setRowSorter(sorter);
+		// Layout the GUI
+		JButton button = new JButton("Select Highlighted Row");
+		JPanel panel = new JPanel();
+		panel.setMaximumSize(new Dimension(30, 30));
+		panel.add(button);
+		add(panel, BorderLayout.WEST);
 
-    }
-  }
+		// Listen to the button click
+		button.addActionListener(new ButtonListener());
+	}
+
+	private class ButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent ae) {
+			// TODO: 8) Show the name of the student on the currently selected
+			// row need table's getSelectedRow and convertRowIndexToModel as
+			// well as model's getValueAt(rowIndex, columnIndex). See the API
+			// for details.
+			int selectRow = table.getSelectedRow();
+			int s = table.convertRowIndexToModel(selectRow);
+			String result = (String) model.getValueAt(s, 0);
+			System.out.println(result);
+
+		}
+	}
 }
